ee74e5cfc04563171ffffe13ca9add0986fc069a
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-26 14:14:27 +0100

    first commit

---
+++ apps/www/src/app/layout.tsx
@@ -0,0 +1,50 @@
+import { Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
+
+import "~/styles/globals.css";
+
+import { cn } from "@saasfly/ui";
+import { Toaster } from "@saasfly/ui/toaster";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+}); // Font files can be colocated inside of `pages`
+const fontHeading = localFont({
+  src: "../styles/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      {/*<Suspense>*/}
+      {/*  <PostHogPageview />*/}
+      {/*</Suspense>*/}
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable,
+        )}
+      >
+        {children}
+        {/* <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+        >
+          <NextDevtoolsProvider>{children}</NextDevtoolsProvider>
+          <Analytics />
+          <SpeedInsights />
+          <Toaster />
+          <TailwindIndicator />
+        </ThemeProvider> */}
+      </body>
+    </html>
+  );
+}
