706a09848553fedc1572ea27de8a0678a98c9462
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-26 18:34:04 +0100

    :tada:

--- apps/www/app/(clean-code)/(sales)/sales-book/(form)/layout.tsx
+++ apps/www/app/(clean-code)/(sales)/sales-book/(form)/layout.tsx
@@ -1,12 +1,13 @@
+import AuthGuard from "@/app/(v2)/(loggedIn)/_components/auth-guard";
 import { CustomerProfileTaxUpdatePrompt } from "@/components/forms/sales-form/customer-profile-tax-update-prompt";
 import { SalesFormFeatureSwitch } from "@/components/sales-form-feature-switch";
 
 export default function Layout({ children }) {
     return (
-        <>
+        <AuthGuard can={["viewSales"]}>
             <SalesFormFeatureSwitch />
             <CustomerProfileTaxUpdatePrompt />
             {children}
-        </>
+        </AuthGuard>
     );
 }
