7bf2b264f62e045f25cbb0968efa23284a0fa1b5
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-04-01 14:31:39 +0100

    :tada:

--- packages/email/emails/dispatch-driver.tsx
+++ packages/email/emails/dispatch-driver.tsx
@@ -1,3 +1,73 @@
+import {
+  Body,
+  Button,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Html,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
 export const DispatchDriver = ({}) => {
-  return <></>;
+  return (
+    <Html>
+      <Tailwind>
+        <head>
+          <Font
+            fontFamily="Geist"
+            fallbackFontFamily="Helvetica"
+            webFont={{
+              url: "https://cdn.jsdelivr.net/npm/@fontsource/geist-sans@5.0.1/files/geist-sans-latin-400-normal.woff2",
+              format: "woff2",
+            }}
+            fontWeight={400}
+            fontStyle="normal"
+          />
+
+          <Font
+            fontFamily="Geist"
+            fallbackFontFamily="Helvetica"
+            webFont={{
+              url: "https://cdn.jsdelivr.net/npm/@fontsource/geist-sans@5.0.1/files/geist-sans-latin-500-normal.woff2",
+              format: "woff2",
+            }}
+            fontWeight={500}
+            fontStyle="normal"
+          />
+        </head>
+        <Preview>Hello</Preview>
+
+        <Body className="mx-auto my-auto bg-[#fff] font-sans">
+          <Container
+            className="mx-auto my-[40px] max-w-[600px] border-transparent p-[20px] md:border-[#E8E7E1]"
+            style={{ borderStyle: "solid", borderWidth: 1 }}
+          >
+            {/* <Logo /> */}
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-[#121212]"></Heading>
+
+            <Section className="mb-[42px] mt-[32px] text-center">
+              <Button
+                className="border border-solid border-[#121212] bg-transparent px-6 py-3 text-center text-[14px] font-medium text-[#121212] text-primary no-underline"
+                // href={inviteLink}
+              >
+                {/* {t("invite.join")} */}
+              </Button>
+            </Section>
+
+            <br />
+            <Section></Section>
+
+            <br />
+
+            {/* <Footer /> */}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
