f95d78330a6f5dd56d7747eb370292975ca9f64d
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-04-04 18:17:57 +0100

    :tada:

--- apps/www/app/(clean-code)/(sales)/sales-book/(form)/_components/components-section/custom-component.action.tsx
+++ apps/www/app/(clean-code)/(sales)/sales-book/(form)/_components/components-section/custom-component.action.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import { Menu } from "@/components/(clean-code)/menu";
+
 import { useFormDataStore } from "../../_common/_stores/form-data-store";
 import { UseStepContext } from "./ctx";
-import { useState } from "react";
 
 export function CustomComponentAction({ ctx }: { ctx: UseStepContext }) {
     const zus = useFormDataStore();
     const stepForm = ctx.cls.getStepForm();
-    const [customMode, setCustomMode] = useState(stepForm?.meta?.custom);
+    const customMode = stepForm?.meta?.custom;
     // useEffect(() => {
     //     setCustomMode(stepForm?.meta?.custom);
     // }, []);
     return (
-        <Menu.Item onClick={() => {}} icon="add">
+        <Menu.Item
+            onClick={() => {
+                zus.dotUpdate(
+                    `kvStepForm.${ctx.stepUid}.meta.custom`,
+                    !customMode,
+                );
+            }}
+            icon="add"
+        >
             {customMode ? "Disable Custom" : "Enable Custom"}
         </Menu.Item>
     );
