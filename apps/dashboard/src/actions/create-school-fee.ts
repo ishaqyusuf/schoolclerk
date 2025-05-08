80ed2bbb050ac41aa66177563d4ba9339bd9aaf0
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-05-08 18:31:00 +0100

    :tada:

--- apps/dashboard/src/actions/create-school-fee.ts
+++ apps/dashboard/src/actions/create-school-fee.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { transaction } from "@/utils/db";
 import z from "zod";
 
 import { prisma } from "@school-clerk/db";
@@ -10,32 +11,34 @@ import { actionClient } from "./safe-action";
 import { createBillableSchema, createSchoolFeeSchema } from "./schema";
 
 export type CreateBillableForm = z.infer<typeof createSchoolFeeSchema>;
-export async function createSchoolFee(data: CreateBillableForm) {
+export async function createSchoolFee(
+  data: CreateBillableForm,
+  tx: typeof prisma,
+) {
   const profile = await getSaasProfileCookie();
-
-  return await prisma.$transaction(async (tx) => {
-    await tx.fees.create({
-      data: {
-        title: data.title,
-        amount: data.amount,
-        schoolProfileId: profile.schoolId,
-        description: data.description,
-        feeHistory: {
-          create: {
-            amount: data.amount,
-            current: true,
-            schoolSessionId: profile.sessionId,
-            termId: profile.termId,
-          },
+  return await tx.fees.create({
+    data: {
+      title: data.title,
+      amount: data.amount,
+      schoolProfileId: profile.schoolId,
+      description: data.description,
+      feeHistory: {
+        create: {
+          amount: data.amount,
+          current: true,
+          schoolSessionId: profile.sessionId,
+          termId: profile.termId,
         },
       },
-    });
+    },
   });
 }
 export const createSchoolFeeAction = actionClient
   .schema(createBillableSchema)
   .action(async ({ parsedInput: data }) => {
-    const resp = await createSchoolFee(data);
-    revalidatePath("/academic/classes");
-    return resp;
+    return await transaction(async (tx) => {
+      const resp = await createSchoolFee(data, tx);
+      revalidatePath("/academic/classes");
+      return resp;
+    });
   });
