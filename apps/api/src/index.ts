088885fa74c0aeff6e09ecd7aca86d263c7bc9ea
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-23 21:27:46 +0100

    :tada:

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,17 @@
-import { handle } from "hono/vercel";
+// import { handle } from "hono/vercel";
+// import app from "./app";
+
+// const handler = handle(app);
+
+// export const GET = handler;
+// export const POST = handler;
+// export const PATCH = handler;
+// export const PUT = handler;
+// export const OPTIONS = handler;
+
 import app from "./app";
 
-const handler = handle(app);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default {
+  port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
+  fetch: app.fetch,
+};
