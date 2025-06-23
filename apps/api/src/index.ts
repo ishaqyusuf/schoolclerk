9e68a20cc034c4941eaeb5cc97ec8bbde5da186f
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-23 21:29:52 +0100

    :tada:

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,42 @@
-// import { handle } from "hono/vercel";
-// import app from "./app";
+import { Hono } from "hono";
+import type { Context } from "./rest/types";
+import { secureHeaders } from "hono/secure-headers";
+import { cors } from "hono/cors";
+import { trpcServer } from "@hono/trpc-server";
+import { appRouter } from "./trpc/routers/_app";
+import { createTRPCContext } from "./trpc/init";
 
-// const handler = handle(app);
+const app = new Hono<Context>();
 
-// export const GET = handler;
-// export const POST = handler;
-// export const PATCH = handler;
-// export const PUT = handler;
-// export const OPTIONS = handler;
-
-import app from "./app";
-
+app.use(secureHeaders());
+// adsad
+app.use(
+  "/trpc/*",
+  cors({
+    origin: process.env.ALLOWED_API_ORIGINS?.split(",") ?? [],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowHeaders: [
+      "Authorization",
+      "Content-Type",
+      "accept-language",
+      "x-trpc-source",
+      "x-tenant-domain",
+      "x-tenant-session-term-id",
+      "x-user-timezone",
+      "x-user-country",
+    ],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 86400,
+  }),
+);
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: createTRPCContext,
+  }),
+);
+// export default app;
 export default {
   port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
   fetch: app.fetch,
