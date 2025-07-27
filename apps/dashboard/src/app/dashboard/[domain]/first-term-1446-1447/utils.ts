008e978c37c7cf5054399333c79c739429bdfb26
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-27 16:05:14 +0100

    asdasd

--- apps/dashboard/src/app/dashboard/[domain]/first-term-1446-1447/utils.ts
+++ apps/dashboard/src/app/dashboard/[domain]/first-term-1446-1447/utils.ts
@@ -33,4 +33,18 @@ export function sortClassroomStudents<T>(
   }
   return students;
 }
-export const indices = ["1", "2", "3", "4", "5", "6", "7", "8"];
+export const indices = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+];
