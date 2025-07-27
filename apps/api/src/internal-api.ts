1bd50cd303bd44367551eebbc1430b7302dde95d
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-27 15:07:02 +0100

    asdas

--- apps/api/src/internal-api.ts
+++ apps/api/src/internal-api.ts
@@ -13,6 +13,9 @@ import { app } from ".";
 
 // export default handle(app);
 export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
 
 /* -----------------------------------------------------------------------------------------------
  * Serverless
